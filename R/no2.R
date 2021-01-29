fed753da1078c22afe30a6d3cd360c4368f52448
guidofioravanti <guidofioravanti@yahoo.it>
2021-01-29 16:12:39 +0100

    Dati no2, nox, pm10, pm25

---
+++ R/no2.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di no2 per 38 stazioni della regione LAZIO
+#'
+#' @format Un tibble con 8 colonne e 100387 osservazioni
+#'
+#' @usage
+#' no2
+"no2"
---
+++ R/nox.R
@@ -0,0 +1,7 @@
+#' Tibble con i dati di nox per 34 stazioni della regione LAZIO
+#'
+#' @format Un tibble con 8 colonne e 92722 osservazioni
+#'
+#' @usage
+#' nox
+"nox"
