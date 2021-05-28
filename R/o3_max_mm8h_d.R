82ae8883101532715e77b471c4cbe18efc7c9bd7
guidofioravanti <guidofioravanti@yahoo.it>
2021-05-28 07:23:44 +0200

    Aggiornamento dati

--- R/o3_max_mm8h_d.R
+++ R/o3_max_mm8h_d.R
@@ -1,6 +1,6 @@
-#' Tibble con i dati di o3_max_mm8h_d per 25 stazioni della regione LAZIO
+#' Tibble con i dati di o3_max_mm8h_d per 20 stazioni della regione LAZIO
 #'
-#' @format Un tibble con 8 colonne e 60058 osservazioni
+#' @format Un tibble con 8 colonne e 57064 osservazioni
 #'
 #' @usage
 #' o3_max_mm8h_d
--- R/pm10.R
+++ R/pm10.R
@@ -1,6 +1,6 @@
-#' Tibble con i dati di pm10 per 47 stazioni della regione LAZIO
+#' Tibble con i dati di pm10 per 39 stazioni della regione LAZIO
 #'
-#' @format Un tibble con 8 colonne e 110154 osservazioni
+#' @format Un tibble con 8 colonne e 110623 osservazioni
 #'
 #' @usage
 #' pm10
